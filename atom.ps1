3f4a3eb16ed93f16774c38a2f5547a4848fc8855
soukouki <soukouki0@yahoo.co.jp>
2017-02-03 21:52:39 +0900

    Add files via upload

--- atom.ps1
+++ atom.ps1
@@ -1,17 +1,19 @@
 ﻿
 
-$atom = (join-path $home \AppData\Local\atom\bin\atom.cmd)
+$atom = (join-path $home "\AppData\Local\atom\bin\atom.cmd")
+
+$path = $ARGS[0]
 
 # 引数がない場合は現在のディレクトリを開く
-if($ARGS.Length -eq 0){
+if($path -eq $null){
 	start $atom (pwd)
 	exit
 }
 
 # 引数[0]が絶対パスならそのパスを開く
-if(Split-Path $ARGS[0] -isAbsolute){
+if(Split-Path $path -isAbsolute){
 	start $atom $ARGS[0]
 # 違えば現在のパスからの引数[0]の相対パスを開く
 }else{
-	start $atom (join-path (pwd) $ARGS[0])
+	start $atom (join-path (pwd) $path)
 }
---
+++ gs.cmd
@@ -0,0 +1,1 @@
+start powershell -noexit "(join-path $home \"/AppData/Local/Github/shell.ps1\"); cd d:/"
