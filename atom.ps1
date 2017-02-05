d2b855ac75bc085a32c1386117878a737ba95b49
soukouki <soukouki0@yahoo.co.jp>
2017-02-05 14:01:14 +0900

    atomの改修とgsの単純化
    gitに環境変数pathを通せばgtも動く

--- atom.ps1
+++ atom.ps1
@@ -4,16 +4,12 @@ $atom = (join-path $home "\AppData\Local\atom\bin\atom.cmd")
 
 $path = $ARGS[0]
 
-# 引数がない場合は現在のディレクトリを開く
-if($path -eq $null){
+if($path -eq $null) { # 引数がない場合は現在のディレクトリを開く
 	start $atom (pwd)
-	exit
-}
-
-# 引数[0]が絶対パスならそのパスを開く
-if(Split-Path $path -isAbsolute){
+} elseif($path.contains("*")) { # パスに「*」が入ったときはlsで探して開く
+	start $atom (ls $path)
+} elseif(Split-Path $path -isAbsolute) { # pathが絶対パスならそのパスを開く
 	start $atom $ARGS[0]
-# 違えば現在のパスからの引数[0]の相対パスを開く
-}else{
+} else { # 違えば現在のパスからの引数[0]の相対パスを開く
 	start $atom (join-path (pwd) $path)
 }
--- gs.cmd
+++ gs.cmd
@@ -1,1 +1,1 @@
-start powershell -noexit "Invoke-Expression (join-path $home \"/AppData/Local/Github/shell.ps1\"); cd d:/"
+start powershell -noexit "cd d:/"
