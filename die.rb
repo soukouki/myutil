ab2b950a391152bb800ef157b102512d61b70adb
soukouki <soukouki0@yahoo.co.jp>
2017-09-30 03:30:04 +0900

    やりすぎた感は否めない。アルゴリズムが非効率かも。

--- die.rb
+++ die.rb
@@ -1,9 +1,28 @@
 
-x, y, z = (ARGV[0] || gets)
-	.chomp
-	.match(/(\d+)d(\d+)((\+|-)\d+|)/)
-	.captures
-	.take(3)
-	.map{|a|(a!="")? a.to_i : 0}
+def calc s
+	x, y, z = s
+		.match(/(\d+)d(\d+)((?:\+|-)\d+)?/)
+		.captures
+		.map(&:to_i)
+	
+	mean = ((y+1)*x/2.0)+z
+	
+	puts "mean:#{mean}"
+	puts "max :#{x*y+z}"
+	puts "min :#{x+z}"
+	
+	return if (y**x)>1000_0000 # このアルゴリズムの場合解析に時間がかかりすぎるため、標準偏差の計算をスキップする。
+	
+	dist = (1..y).to_a
+		.repeated_permutation(x)
+		.map{|arr|arr.inject(:+)}
+		.inject({}){|hash, n|hash[n] = (hash[n]||0)+1; hash}
+	
+	puts "standard deviation:#{dist.map{|key, count|((key-mean)**2)*count}.inject(:+)**0.5}"
+end
 
-puts [0.0, *(1..y)].inject(&:+)/y*x+z
+if ARGV[0].nil?
+	calc(gets)
+else
+	ARGV.each{|s|calc(s)}
+end
