664c34616b7c61f4c2afbea6228a27e8d2f83e78
soukouki <soukouki0@yahoo.co.jp>
2017-09-30 02:28:08 +0900

    softalk.rb softalkが起動していないときにずっと止まってしまうバグを修正

--- softalk.rb
+++ softalk.rb
@@ -1,3 +1,14 @@
-
+﻿
 # 実行する際は、改行を`\\n`にすること
-`start #{File.expand_path(File.dirname(__FILE__))}/softalk/softalk.exe "/W:#{ARGV[0].dup.force_encoding("cp932").gsub("\\n"){"。".encode("cp932")}}"`
+
+softalk_path = File.expand_path(File.dirname(__FILE__))+"/softalk/softalk.exe"
+
+softalk_args =
+	if ARGV[0].nil?
+		""
+	else
+		text = ARGV[0].dup.force_encoding("cp932").gsub("\\n"){"。".encode("cp932")}
+		"/W:"+text
+	end
+
+Thread.new{`start #{softalk_path} #{softalk_args}`}
